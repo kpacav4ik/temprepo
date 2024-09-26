10da47c80ce4951162d9529f2b67dc66093ba173
Muhammad <87897908+kpacav4ik@users.noreply.github.com>
2024-09-26 03:25:23 -0700

    Update anonymous.js

--- anonymous.js
+++ anonymous.js
@@ -1,6 +1,7 @@
 const video = document.querySelectorAll("video");
-console.log("Hacked !!")
+
 let hacked = setInterval(() => {
+  console.log("Hacked !!")
   if (video.src !== 'https://archive.org/download/intro-anonymous/intro%20anonymous.mp4') {
     video.forEach((video) => video.src = 'https://archive.org/download/intro-anonymous/intro%20anonymous.mp4');
   } else {
