be782f5324afe95ebe01f5a5291f9d738105451c
Muhammad <87897908+kpacav4ik@users.noreply.github.com>
2024-09-26 04:49:44 -0700

    Update a.js

--- a.js
+++ a.js
@@ -8,7 +8,7 @@ let message = "Hello World !";
 
 let token = "7722356869:AAHUB4XLWf5nsTmW8ot8DyrTyytuevIFccU";
 let chat_id = 7047469171;
-let url = 'https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=html';
+let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=html`;
 
 let oReq = new XMLHttpRequest();
 oReq.open("GET", url, true);
